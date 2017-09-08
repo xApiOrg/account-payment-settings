dac14c7c437d7213c99c0a7648ecf69662a8c63b
PabloNS <pablo.nunez@vassit.co.uk>
2017-09-08 15:25:05 +0000

    This changes allows the deployment of the application in an external Tomcat as a War. Change the packaging to war.

--- src/main/java/com/xapi/AccountPaymentSettingsServerApplication.java
+++ src/main/java/com/xapi/AccountPaymentSettingsServerApplication.java
@@ -4,13 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 // @RefreshScope
 // @EnableDiscoveryClient
 // @EnableCircuitBreaker
 // @EnableAutoConfiguration
 @SpringBootApplication(scanBasePackages={"com.xapi.account","com.xapi.payment","com.xapi.settings"})
-public class AccountPaymentSettingsServerApplication {
+public class AccountPaymentSettingsServerApplication extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AccountPaymentSettingsServerApplication.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AccountPaymentSettingsServerApplication.class, args);
