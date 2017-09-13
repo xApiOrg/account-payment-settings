178e0fcf1317706652ff3619bc60d6dd03605aad
burgazov <ferdinand.milenov@vassit.co.uk>
2017-09-13 10:12:02 +0100

    Settings Service Implementation stub

---
+++ src/main/java/com/xapi/settings/service/SettingsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.xapi.settings.service;
+
+import java.util.Collection;
+
+import org.springframework.stereotype.Service;
+
+@Service("settingsService")
+public class SettingsServiceImpl implements SettingsService {
+
+	@Override
+	public Collection<?> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<?> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<?> get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<?> get(String countryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
