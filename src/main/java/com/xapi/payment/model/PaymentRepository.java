6c0109a818829ee735d9c47a99e4d39de3ebb504
burgazov <ferdinand.milenov@vassit.co.uk>
2017-09-13 10:10:32 +0100

    Payment Repository

---
+++ src/main/java/com/xapi/payment/model/PaymentRepository.java
@@ -0,0 +1,11 @@
+package com.xapi.payment.model;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+//	JpaRepository, CrudRepository
+	List<Payment> findByUserId(Long userId);
+}
