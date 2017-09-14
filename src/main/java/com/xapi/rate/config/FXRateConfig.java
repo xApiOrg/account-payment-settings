4e63962f934a3160e390bb747ca268b62684ce40
burgazov <ferdinand.milenov@vassit.co.uk>
2017-09-14 11:08:31 +0100

    Charge and FX rate configuration awaiting points

---
+++ src/main/java/com/xapi/rate/config/FXRateConfig.java
@@ -0,0 +1,13 @@
+package com.xapi.rate.config;
+
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+//@RefreshScope
+public class FXRateConfig {
+//	@Value("${exchange.apiProtocol}") private String apiProtocol;
+//	@Value("${exchange.apiDomain}") private String apiDomain;
+//	@Value("${exchange.apiPath}") private String apiPath;
+}
