785bcb6017a1b430000126fce9f648a7fb3737e9
Manuel Ferran Pousa <manuel.ferran@vassit.co.uk>
2017-09-15 16:54:45 +0100

    Final release of run_linux bash script

--- src/main/docker/run_linux.sh
+++ src/main/docker/run_linux.sh
@@ -1,3 +1,31 @@
-kill $(cat /tmp/instance.pid)
-java -jar ../@project.build.finalName@.jar 1> out.log &
-echo $! > /tmp/instance.pid
+copyFile="/tmp/ipay.sh"
+fileName="$(basename "$0")"
+qualifiedPath="$(dirname "$(readlink -f "$0")")/$fileName"
+
+function killInstance {
+    echo "[INFO] Killing previous instance"
+    pkill -f ipayService
+    if [[ $! -eq 0 ]]
+    then echo "[OK] Instance killed"; 
+    else echo "[FAIL] No instance to kill"; 
+    fi
+}
+function runInstance {
+    echo "[INFO] Running instance" >> /tmp/ipay.instance.log
+    exec -a ipayService java -jar ./target/xapi-account-payment-settings-0.0.1-SNAPSHOT.jar 1>> /tmp/ipay.instance.log 2>> /tmp/ipay.instance.log &
+    echo "[OK] Async process launched" >> /tmp/ipay.instance.log
+}
+
+if [[ "$qualifiedPath" != "$copyFile" ]]
+then
+    echo "[INFO] Redirecting to $copyFile $@"
+    cp -f "$qualifiedPath" "$copyFile"
+    exec bash $copyFile $@
+else
+    echo "[INFO] Running $fileName $@"
+    for param in "$@"
+    do
+        if [[ "$param" = "-k" ]]; then killInstance; fi
+        if [[ "$param" = "-r" ]]; then runInstance; fi
+    done
+fi
