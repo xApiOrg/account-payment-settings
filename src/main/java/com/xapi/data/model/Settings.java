f3551606a72c7ec11fec5de0305e621d451f2cbf
burgazov <ferdinand.milenov@vassit.co.uk>
2017-09-27 11:39:10 +0100

    Settings and Section elements

---
+++ src/main/java/com/xapi/data/model/Settings.java
@@ -0,0 +1,63 @@
+package com.xapi.data.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Settings implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private long id;
+	private String name;
+	private String code;
+	private String flag;
+	private String currency;
+	private List<Section> sections;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getFlag() {
+		return flag;
+	}
+
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public List<Section> getSections() {
+		return sections;
+	}
+
+	public void setSections(List<Section> sections) {
+		this.sections = sections;
+	}
+}
