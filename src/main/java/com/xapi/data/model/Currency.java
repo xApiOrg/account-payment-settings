6330457521a63f551ede3119d4ba2cc2772f0ab1
burgazov <ferdinand.milenov@vassit.co.uk>
2017-10-31 16:17:31 +0000

    Table and domain class currency introduced

---
+++ src/main/java/com/xapi/data/model/Currency.java
@@ -0,0 +1,38 @@
+package com.xapi.data.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="currency")
+public class Currency  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="ISO", nullable=false, unique=true) private String iso;
+	@Column(name="NAME", nullable=false, unique=true) private String name;
+	@Column(name="SYMBOL", nullable=false, unique=true) private String symbol;
+	
+	public String getIso() {
+		return iso;
+	}
+	public void setIso(String iso) {
+		this.iso = iso;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+}
