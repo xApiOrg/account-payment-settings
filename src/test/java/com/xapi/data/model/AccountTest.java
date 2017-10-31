2499084dab654016fa001b38c2ba67f8d3dccbdb
burgazov <ferdinand.milenov@vassit.co.uk>
2017-10-31 15:47:48 +0000

    Account integration test

---
+++ src/test/java/com/xapi/data/model/AccountTest.java
@@ -0,0 +1,135 @@
+package com.xapi.data.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class AccountTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	 
+	@Test
+  	public void fieldAnnotations() {
+//		Needs helper class AssertAnnotations '₱'
+//		AssertAnnotations.assertField(Account.class, "id");
+//		AssertAnnotations.assertField(Account.class, "firstName");
+//		AssertAnnotations.assertField(Account.class, "lastName");
+//		AssertAnnotations.assertField(Account.class, "phones");
+	}
+
+	@Test
+	public final void testAccount() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAccountUser() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetUser() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetId() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetId() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetBalance() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetBalance() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetOverDraft() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetOverDraft() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetType() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetType() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetPayments() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetPayments() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testToString() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCurrency() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetCurrency() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCreated() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetAccountDetails() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetAccountDetails() {
+		// fail("Not yet implemented"); // TODO
+	}
+
+}
