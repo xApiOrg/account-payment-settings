8151236beb3128033c3fc82ddfc7ee7d21b1c333
burgazov <ferdinand.milenov@vassit.co.uk>
2017-11-02 17:00:31 +0000

    Code clean up

--- src/main/java/com/xapi/data/model/AccountDetails.java
+++ src/main/java/com/xapi/data/model/AccountDetails.java
@@ -17,12 +17,9 @@ public class AccountDetails implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id @GeneratedValue 							private Long id;
-//	@Column(name="IDETIFIER",nullable=false) 		private final String idetifier; // OBSOLETE and unclear
-//	@Column(name="ISO_COUNTRY_CODE",nullable=false)	private final String isoCountryCode; // GOES to Bank
 	@Column(name="BANK_IDENTIFIER",nullable=false)	private final String bankIdentifier;
 	@Column(name="BRANCH_IDENTIFIER",nullable=false)private final String branchIdentifier;
 	@Column(name="ACCOUNT_NUMBER",nullable=false)	private final String accountNumber;
-//	@Column(name="SEPA_MEMBER",nullable=false)		private final Boolean sepaMember; // GOES to Bank
 	
     @OneToOne(fetch = FetchType.EAGER) @JoinColumn(name = "account_id")
     												private Account account;
@@ -31,12 +28,8 @@ public class AccountDetails implements Serializable {
     												private Payee payee;
 	
 	public AccountDetails(	String bankIdentifier, String branchIdentifier, String accountNumber ){ 
-		//String idetifier, String isoCountryCode, Boolean sepaMember
-		
-//		this.isoCountryCode = idetifier != null && idetifier.length() > 2? idetifier.substring(0, 2): "";
 		this.bankIdentifier = bankIdentifier; this.branchIdentifier = branchIdentifier;
 		this.accountNumber = accountNumber; 
-		// this.sepaMember = sepaMember; this.idetifier = idetifier; 
 	}
 
 	public Long getId() {
@@ -46,14 +39,6 @@ public class AccountDetails implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-//	public String getIdetifier() {
-//		return idetifier;
-//	}
-
-//	public String getIsoCountryCode() {
-//		return isoCountryCode;
-//	}
 
 	public String getBankIdentifier() {
 		return bankIdentifier;
@@ -65,12 +50,7 @@ public class AccountDetails implements Serializable {
 
 	public String getAccountNumber() {
 		return accountNumber;
-	}
-
-//	public Boolean getSepaMember() {
-//		return sepaMember;
-//	}	
-	
+	}	
 }
 // https://stackoverflow.com/questions/1493229/multiple-yet-mutually-exclusive-foreign-keys-is-this-the-way-to-go
 // https://dba.stackexchange.com/questions/42434/database-design-two-1-to-many-relationships-to-the-same-table
