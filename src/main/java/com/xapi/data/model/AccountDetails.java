dea98be0bb7d64e8e286de34da0309953bea8cdb
burgazov <ferdinand.milenov@vassit.co.uk>
2017-11-02 16:58:28 +0000

    Commented out some fragments. idetifier dropped off, not clear and seems obsolete isoCountryCode and sepaMember go to Bank

--- src/main/java/com/xapi/data/model/AccountDetails.java
+++ src/main/java/com/xapi/data/model/AccountDetails.java
@@ -17,12 +17,12 @@ public class AccountDetails implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id @GeneratedValue 							private Long id;
-	@Column(name="IDETIFIER",nullable=false) 		private final String idetifier;
-	@Column(name="ISO_COUNTRY_CODE",nullable=false)	private final String isoCountryCode;
+//	@Column(name="IDETIFIER",nullable=false) 		private final String idetifier; // OBSOLETE and unclear
+//	@Column(name="ISO_COUNTRY_CODE",nullable=false)	private final String isoCountryCode; // GOES to Bank
 	@Column(name="BANK_IDENTIFIER",nullable=false)	private final String bankIdentifier;
 	@Column(name="BRANCH_IDENTIFIER",nullable=false)private final String branchIdentifier;
 	@Column(name="ACCOUNT_NUMBER",nullable=false)	private final String accountNumber;
-	@Column(name="SEPA_MEMBER",nullable=false)		private final Boolean sepaMember;
+//	@Column(name="SEPA_MEMBER",nullable=false)		private final Boolean sepaMember; // GOES to Bank
 	
     @OneToOne(fetch = FetchType.EAGER) @JoinColumn(name = "account_id")
     												private Account account;
@@ -30,12 +30,13 @@ public class AccountDetails implements Serializable {
     @OneToOne(fetch = FetchType.EAGER) @JoinColumn(name = "payee_id")
     												private Payee payee;
 	
-	public AccountDetails(	String idetifier, String bankIdentifier, // String isoCountryCode, 
-							String branchIdentifier, String accountNumber, Boolean sepaMember){
-		this.idetifier = idetifier; 
-		this.isoCountryCode = idetifier != null && idetifier.length() > 2? idetifier.substring(0, 2): "";
+	public AccountDetails(	String bankIdentifier, String branchIdentifier, String accountNumber ){ 
+		//String idetifier, String isoCountryCode, Boolean sepaMember
+		
+//		this.isoCountryCode = idetifier != null && idetifier.length() > 2? idetifier.substring(0, 2): "";
 		this.bankIdentifier = bankIdentifier; this.branchIdentifier = branchIdentifier;
-		this.accountNumber = accountNumber; this.sepaMember = sepaMember; 
+		this.accountNumber = accountNumber; 
+		// this.sepaMember = sepaMember; this.idetifier = idetifier; 
 	}
 
 	public Long getId() {
@@ -46,13 +47,13 @@ public class AccountDetails implements Serializable {
 		this.id = id;
 	}
 
-	public String getIdetifier() {
-		return idetifier;
-	}
+//	public String getIdetifier() {
+//		return idetifier;
+//	}
 
-	public String getIsoCountryCode() {
-		return isoCountryCode;
-	}
+//	public String getIsoCountryCode() {
+//		return isoCountryCode;
+//	}
 
 	public String getBankIdentifier() {
 		return bankIdentifier;
@@ -66,9 +67,10 @@ public class AccountDetails implements Serializable {
 		return accountNumber;
 	}
 
-	public Boolean getSepaMember() {
-		return sepaMember;
-	}	
+//	public Boolean getSepaMember() {
+//		return sepaMember;
+//	}	
+	
 }
 // https://stackoverflow.com/questions/1493229/multiple-yet-mutually-exclusive-foreign-keys-is-this-the-way-to-go
 // https://dba.stackexchange.com/questions/42434/database-design-two-1-to-many-relationships-to-the-same-table
@@ -79,50 +81,3 @@ public class AccountDetails implements Serializable {
 //https://www.bankbazaar.com/ifsc-code/bank-of-india.html, 
 //https://sourceforge.net/p/ifscandmicrcode/wiki/Home/
 
-/*
-Your IBAN Number	BG38 RZBB 9155 4027 1608 04
-ISO Country Code	BG (Bulgaria)
-IBAN Check Digits	38
-BBAN	RZBB 9155 4027 1608 04
-Bank Identifier	RZBB
-Branch Identifier	9155
-Account Number	27160804
-BBAN Check Digit(s)	40
-SEPA Member	Yes
-
-Your IBAN Number	BG50 CITI 9250 440C 9TEI AI
-ISO Country Code	BG (Bulgaria)
-IBAN Check Digits	50
-BBAN	CITI 9250 440C 9TEI AI
-Bank Identifier	CITI
-Branch Identifier	9250
-Account Number	0C9TEIAI
-BBAN Check Digit(s)	44
-SEPA Member	Yes
-
-Your IBAN Number	CH61 0076 7000 S521 7090 4
-ISO Country Code	CH (Switzerland)
-IBAN Check Digits	61
-BBAN	0076 7000 S521 7090 4
-Bank Identifier	00767
-Account Number	000S52170904
-SEPA Member	Yes
-
-Your IBAN Number	GB75 BARC 2004 1568 0883 06
-ISO Country Code	GB (United Kingdom)
-IBAN Check Digits	75
-BBAN	BARC 2004 1568 0883 06
-Bank Identifier	BARC
-Branch Identifier	200415
-Account Number	68088306
-SEPA Member	Yes
-
-Your IBAN Number	GB11 LOYD 3094 6610 4015 60
-ISO Country Code	GB (United Kingdom)
-IBAN Check Digits	11
-BBAN	LOYD 3094 6610 4015 60
-Bank Identifier	LOYD
-Branch Identifier	309466
-Account Number	10401560
-SEPA Member	Yes
-*/
